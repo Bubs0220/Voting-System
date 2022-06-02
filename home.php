65fb1734f9ebde16263059168035d07c63741d4d
glygngy <94152686+glygngy@users.noreply.github.com>
2022-06-02 13:57:02 +0800

    landing page
    design

---
+++ home.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Voting system</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Anton&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arbutus&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Archivo+Black&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+</head>
+
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="54" style="color: rgb(33, 37, 41);">
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
+        <div class="container"><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive"><a class="navbar-brand" href="#page-top" style="color: var(--bs-red);font-family: 'Archivo Black', sans-serif;">VS-TUP</a>
+                <ul class="navbar-nav ms-auto text-uppercase">
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="masthead" style="background-image: url('assets/img/header-bg.jpg');height: 1132px;">
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in" style="font-size: 50px;"><span style="font-size: 107px;font-family: ABeeZee, sans-serif;margin: 0px;text-shadow: 6px -1px 0px var(--bs-gray-900);font-weight: bold;color: var(--bs-red);">VOTING SYSTEM</span></div>
+                <div class="intro-heading text-uppercase" style="color: var(--bs-white);font-size: 78px;margin: 103px;">
+                <span style="font-size: 60px;text-shadow: 6px 0px var(--bs-body-color);color: var(--bs-body-bg);">
+                in technological university of the philippines</span>
+            </div>
+            <a class="btn btn-primary btn-xl text-uppercase" role="button" href="login.php" style="background: #633330;color: var(--bs-white);margin: 16px;">
+                login</a>
+            </div>
+        </div>
+    </header>
+    <section class="bg-light" id="portfolio" style="height: 1694.44px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">Portfolio</h2>
+                    <h3 class="text-muted section-subheading">Images from Technological University of the Philippines</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-md-4 portfolio-item"><a class="portfolio-link" href="#portfolioModal1" data-bs-toggle="modal"><img class="img-fluid" src="assets/img/portfolio/1-thumbnail.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>University logo</h4>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item"><a class="portfolio-link" href="#portfolioModal2" data-bs-toggle="modal"><img class="img-fluid" src="assets/img/portfolio/2-thumbnail.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>Architecture Design</h4>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item"><a class="portfolio-link" href="#portfolioModal3" data-bs-toggle="modal"><img class="img-fluid" src="assets/img/portfolio/3-thumbnail.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>Hallway</h4>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item"><a class="portfolio-link" href="#portfolioModal4" data-bs-toggle="modal"><img class="img-fluid" src="assets/img/portfolio/4-thumbnail.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>Stairs</h4>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item"><a class="portfolio-link" href="#portfolioModal5" data-bs-toggle="modal"><img class="img-fluid" src="assets/img/portfolio/5-thumbnail.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>University Main Gate</h4>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 portfolio-item"><a class="portfolio-link" href="#portfolioModal6" data-bs-toggle="modal"><img class="img-fluid" src="assets/img/portfolio/6-thumbnail.jpg"></a>
+                    <div class="portfolio-caption">
+                        <h4>Court</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="bg-light" id="team" style="background: var(--bs-pink);">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="assets/img/team/archie.jpg">
+                        <h4>Archie Dantes</h4>
+                        <p class="text-muted">Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li class="list-inline-item"></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="assets/img/team/ching.jpg">
+                        <h4>Caryl Mickyle Ching</h4>
+                        <p class="text-muted">Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li class="list-inline-item"></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="team-member"><img class="rounded-circle mx-auto" src="assets/img/team/kervin.jpg">
+                        <h4>Kervin Evangelista</h4>
+                        <p class="text-muted">Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li class="list-inline-item"></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="bg-light" id="team-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="team-member"><img class="rounded-circle mx-auto" src="assets/img/team/gangay.jpg">
+                            <h4>Angela Gangay</h4>
+                            <p class="text-muted">UI Designer</p>
+                            <ul class="list-inline social-buttons">
+                                <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li class="list-inline-item"></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="team-member"><img class="rounded-circle mx-auto" src="assets/img/team/cha.jpg">
+                            <h4>Charlotte Gimena</h4>
+                            <p class="text-muted">UI Designer</p>
+                            <ul class="list-inline social-buttons">
+                                <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li class="list-inline-item"></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="team-member"><img class="rounded-circle mx-auto" src="assets/img/team/jas.jpg">
+                            <h4>Jasmhine Modrigo</h4>
+                            <p class="text-muted">UI Designer</p>
+                            <ul class="list-inline social-buttons">
+                                <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li class="list-inline-item"></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <section id="contact" style="background-image: url('assets/img/map-image.png');">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">Contact Us</h2>
+                    <h3 class="text-muted section-subheading"></h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <form id="contactForm" name="contactForm" novalidate="novalidate">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group mb-3"><input class="form-control" type="text" id="name" placeholder="Your Name *" required=""><small class="form-text text-danger flex-grow-1 help-block lead"></small></div>
+                                <div class="form-group mb-3"><input class="form-control" type="email" id="email" placeholder="Your Email *" required=""><small class="form-text text-danger help-block lead"></small></div>
+                                <div class="form-group mb-3"><input class="form-control" type="tel" placeholder="Your Phone *" required=""><small class="form-text text-danger help-block lead"></small></div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group mb-3"><textarea class="form-control" id="message" placeholder="Your Message *" required="">
+
+                                </textarea>
+                                <small class="form-text text-danger help-block lead"></small></div>
+                            </div>
+                            <div class="w-100"></div>
+                            <div class="col-lg-12 text-center">
+                                <div id="success"></div><button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit" style="color: rgb(255, 255, 255);background: var(--bs-red);">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><span class="copyright">Voting System 2022</span></div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item"></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <ul class="list-inline quicklinks">
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a href="#">Terms of Use</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/agency.js"></script>
+</body>
+
+</html>
